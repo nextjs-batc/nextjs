595f289fb1bca66d95248a12c3e9eba97da05ce6
JarvisAppleSlice <Samuel.s.jarvis@gmail.com>
2026-05-24 17:47:54 -0600

    Update page.tsx

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,6 +1,8 @@
+import { Suspense } from "react";
 import { fetchCustomers } from "@/app/lib/data";
 import CustomersTable from "@/app/ui/customers/table";
 import { Metadata } from "next";
+import Search from "@/app/ui/search";
 
 export const metadata: Metadata = {
 	title: "Customers",
@@ -12,6 +14,11 @@ export default async function Page() {
 	return (
 		<div>
 			<h1 className="text-2xl font-semibold mb-4">Customers</h1>
+
+			<Suspense fallback={<div>Loading search...</div>}>
+				<Search placeholder="Search customers..." />
+			</Suspense>
+
 			<CustomersTable customers={customers} />
 		</div>
 	);
