8882df05cd75f881b7c94cd2b60088afbf907ec8
JarvisAppleSlice <Samuel.s.jarvis@gmail.com>
2026-05-24 15:04:48 -0600

    Finish 13

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -30,12 +30,14 @@ export type State = {
 };
 
 export async function createInvoice(prevState: State, formData: FormData) {
+	// Validate form using Zod
 	const validatedFields = CreateInvoice.safeParse({
 		customerId: formData.get("customerId"),
 		amount: formData.get("amount"),
 		status: formData.get("status"),
 	});
 
+	// If form validation fails, return errors early. Otherwise, continue.
 	if (!validatedFields.success) {
 		return {
 			errors: validatedFields.error.flatten().fieldErrors,
@@ -43,27 +45,27 @@ export async function createInvoice(prevState: State, formData: FormData) {
 		};
 	}
 
+	// Prepare data for insertion into the database
 	const { customerId, amount, status } = validatedFields.data;
 	const amountInCents = amount * 100;
 	const date = new Date().toISOString().split("T")[0];
 
+	// Insert data into the database
 	try {
 		await sql`
       INSERT INTO invoices (customer_id, amount, status, date)
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
 	} catch (error) {
-		// We'll also log the error to the console for now
-		console.error(error);
+		// If a database error occurs, return a more specific error.
 		return {
 			message: "Database Error: Failed to Create Invoice.",
 		};
 	}
 
+	// Revalidate the cache for the invoices page and redirect the user.
 	revalidatePath("/dashboard/invoices");
 	redirect("/dashboard/invoices");
-	// Test it out:
-	// console.log(FormData);
 }
 
 export async function updateInvoice(id: string, prevState: State, formData: FormData) {
@@ -85,15 +87,12 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
 
 	try {
 		await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
 	} catch (error) {
-		console.error(error);
-		return {
-			message: "Database Error: Failed to Update Invoice.",
-		};
+		return { message: "Database Error: Failed to Update Invoice." };
 	}
 
 	revalidatePath("/dashboard/invoices");
--- nextjs-dashboard/app/ui/invoices/create-form.tsx
+++ nextjs-dashboard/app/ui/invoices/create-form.tsx
@@ -130,6 +130,11 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
 								</p>
 							))}
 					</div>
+					<div id="status-error" aria-live="polite" aria-atomic="true">
+						{state.message ? (
+							<p className="mt-2 text-sm text-red-500">{state.message}</p>
+						) : null}
+					</div>
 				</fieldset>
 			</div>
 			<div className="mt-6 flex justify-end gap-4">
