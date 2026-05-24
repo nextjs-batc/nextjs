ff8b46181cb1137c8d109a7f85c3f55e185e7de5
JarvisAppleSlice <Samuel.s.jarvis@gmail.com>
2026-05-24 16:46:02 -0600

    Metadata Update

--- nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
@@ -5,7 +5,7 @@ import { notFound } from "next/navigation";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-	title: "Update",
+	title: "Update Invoice",
 };
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
