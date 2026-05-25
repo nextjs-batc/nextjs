08c36b9902d26ac8b251b7056528cc6ef292393c
JarvisAppleSlice <Samuel.s.jarvis@gmail.com>
2026-05-24 19:35:17 -0600

    update

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,25 +1,34 @@
-import { fetchCustomers } from "@/app/lib/data";
+import { fetchFilteredCustomers } from "@/app/lib/data";
 import CustomersTable from "@/app/ui/customers/table";
 import { Metadata } from "next";
 import Search from "@/app/ui/search";
 import { Suspense } from "react";
+import { lusitana } from "@/app/ui/fonts";
 
 export const metadata: Metadata = {
 	title: "Customers",
 };
 
-export default async function Page() {
-	const customers = await fetchCustomers();
+export default async function Page({
+	searchParams,
+}: {
+	searchParams?: Promise<{ query?: string }>;
+}) {
+	const params = await searchParams;
+	const query = params?.query ?? "";
+	const customers = await fetchFilteredCustomers(query);
 
 	return (
-		<div>
-			<h1 className="text-2xl font-semibold mb-4">Customers</h1>
-
-			<Suspense fallback={<div>Loading search...</div>}>
-				<Search placeholder="Search customers..." />
+		<div className="w-full">
+			<div className="flex w-full items-center justify-between">
+				<h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+			</div>
+			<div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+				<Search placeholder="Search Customers..." />
+			</div>
+			<Suspense fallback={<div>Loading Customers...</div>}>
+				<CustomersTable customers={customers} />
 			</Suspense>
-
-			<CustomersTable customers={customers} />
 		</div>
 	);
 }
--- nextjs-dashboard/app/ui/customers/table.tsx
+++ nextjs-dashboard/app/ui/customers/table.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import { lusitana } from "@/app/ui/fonts";
-import Search from "@/app/ui/search";
-import { CustomersTableType, FormattedCustomersTable } from "@/app/lib/definitions";
+import { FormattedCustomersTable } from "@/app/lib/definitions";
 
 export default async function CustomersTable({
 	customers,
@@ -10,8 +9,6 @@ export default async function CustomersTable({
 }) {
 	return (
 		<div className="w-full">
-			<h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>Customers</h1>
-			<Search placeholder="Search customers..." />
 			<div className="mt-6 flow-root">
 				<div className="overflow-x-auto">
 					<div className="inline-block min-w-full align-middle">
@@ -52,6 +49,7 @@ export default async function CustomersTable({
 									</div>
 								))}
 							</div>
+
 							<table className="hidden min-w-full rounded-md text-gray-900 md:table">
 								<thead className="rounded-md bg-gray-50 text-left text-sm font-normal">
 									<tr>
